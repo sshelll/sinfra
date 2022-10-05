f13261cac33319be1227aacc4e47b71a5d12f5ba
shaojiale <sjl66666666@gmail.com>
2022-10-05 18:06:30 +0800

    update: add more ptr func

--- conv/ptr.go
+++ conv/ptr.go
@@ -7,6 +7,10 @@ func Ptr(i interface{}) interface{} {
 	return &i
 }
 
+func BoolPtr(b bool) *bool {
+	return &b
+}
+
 func IntPtr(i int) *int {
 	return &i
 }
@@ -31,6 +35,47 @@ func Int64Ptr(i int64) *int64 {
 	return &i
 }
 
+func UintPtr(u uint) *uint {
+	return &u
+}
+
+func Uint8Ptr(u uint8) *uint8 {
+	return &u
+}
+
+func Uint16Ptr(u uint16) *uint16 {
+	return &u
+}
+
+func Uint32Ptr(u uint32) *uint32 {
+	return &u
+}
+
+func Uint64Ptr(u uint64) *uint64 {
+	return &u
+}
+
 func StrPtr(s string) *string {
 	return &s
+}
+
+func IntElemOrDefault(p *int, i int) int {
+	if p != nil {
+		return *p
+	}
+	return i
+}
+
+func Int64ElemOrDefault(p *int64, i int64) int64 {
+	if p != nil {
+		return *p
+	}
+	return i
+}
+
+func StrElemOrDefault(p *string, s string) string {
+	if p != nil {
+		return *p
+	}
+	return s
 }
