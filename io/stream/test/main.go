6d8b66b2d16095d94b27097808ea7e0277628129
shaojiale <shaojiale@bytedance.com>
2022-11-18 16:30:07 +0800

    update: test stream with more than 1 file

--- io/stream/test/main.go
+++ io/stream/test/main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/SCU-SJL/sinfra/io/stream"
 	"github.com/SCU-SJL/sinfra/io/stream/archive"
@@ -17,7 +16,49 @@ import (
 const uploadURL = `http://localhost:8080/upload`
 
 func main() {
-	testDownloadToZipToUpload()
+	testBatchDownloadToZipToUpload()
+}
+
+func testBatchDownloadToZipToUpload() {
+
+	// data src
+	downloader := NewFileDownloader([]string{"foo.txt", "bar.txt"})
+	iowriter := stream.NewSafeIOStreamWriter(downloader)
+
+	// stream procs
+	zipper := archive.NewZipper()
+	zipper.SetDownstreamCtx(
+		context.WithValue(context.Background(), "filename", "foobar.zip"),
+	)
+	zipper.SetCtxKeyOfFileName("filename")
+
+	multiparter := streamhttp.NewMultiparter()
+	multiparter.SetCtxKeyOfFileName("filename")
+
+	proc := stream.BuildProcChain(zipper.Proc, multiparter.Proc)
+
+	// start proc
+	iostream, errp := iowriter.Start()
+	outStream, outErr := proc(iostream, errp)
+
+	for {
+		data, closed := outStream.Read()
+		if closed {
+			break
+		}
+		rc := data.ReadCloser()
+		uploadFile(rc, multiparter.FormDataContentType())
+		rc.Close()
+	}
+
+	for {
+		err, done := outErr.Check()
+		if done {
+			break
+		}
+		log.Println("out err:", err.Error())
+	}
+
 }
 
 func testDownloadToZipToUpload() {
@@ -35,7 +76,7 @@ func testDownloadToZipToUpload() {
 
 	streamProc := stream.BuildProcChain(zipper.Proc, multiparter.Proc)
 
-	downloadReader := downloadFile()
+	downloadReader := downloadFile("test.txt")
 
 	outputStream, outputErr := streamProc(
 		stream.NewClosedIOStream(stream.NewSimpleDatapack(ctx, downloadReader)),
@@ -45,38 +86,16 @@ func testDownloadToZipToUpload() {
 	datapack, _ := outputStream.Read()
 	rc := datapack.ReadCloser()
 
-	// start uploading
-	req, err := http.NewRequest(http.MethodPost, uploadURL, rc)
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Add("Content-Type", multiparter.FormDataContentType())
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-
-	bs, _ := ioutil.ReadAll(resp.Body)
-	log.Println("upload resp:", string(bs))
+	uploadFile(rc, multiparter.FormDataContentType())
+	rc.Close()
 
 	log.Println("output err =", outputErr.Get())
 
 }
 
-func testDownload() {
-	downloaded := downloadFile()
-	f, err := os.Create("test.txt")
-	if err != nil {
-		panic(err)
-	}
-	io.Copy(f, downloaded)
-	downloaded.Close()
-}
-
-func downloadFile() io.ReadCloser {
-	filename := "test.txt"
-	req, err := http.NewRequest(http.MethodGet, genDownloadURL(filename), nil)
+func downloadFile(filename string) io.ReadCloser {
+	url := fmt.Sprintf("http://localhost:8080/download?filename=%s", filename)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -85,19 +104,52 @@ func downloadFile() io.ReadCloser {
 		log.Fatalf("do http req failed: %v\n", err)
 	}
 	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("do http req not ok, resp = %v\n", string(readHttpResp(resp)))
+		log.Fatalf("do http req not ok, resp = %v\n", readHttpResp(resp))
 	}
 	return resp.Body
 }
 
-func readHttpResp(resp *http.Response) []byte {
+func uploadFile(r io.Reader, contentType string) {
+	req, err := http.NewRequest(http.MethodPost, uploadURL, r)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", contentType)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("upload resp:", readHttpResp(resp))
+}
+
+func readHttpResp(resp *http.Response) string {
 	bs, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	return bs
+	return string(bs)
 }
 
-func genDownloadURL(filename string) string {
-	return fmt.Sprintf("http://localhost:8080/download?filename=%s", filename)
+type FileDownloader struct {
+	idx      int
+	fileList []string
+}
+
+func NewFileDownloader(fileList []string) *FileDownloader {
+	return &FileDownloader{
+		idx:      0,
+		fileList: fileList,
+	}
+}
+
+func (f *FileDownloader) Next() (datapack stream.Datapack, hasNext bool, err error) {
+	filename := f.fileList[f.idx]
+	ctx := context.Background()
+	filepath := fmt.Sprintf("dir%d/%s", f.idx, filename)
+	ctx = context.WithValue(ctx, "filename", filepath)
+	rc := downloadFile(f.fileList[f.idx])
+	f.idx++
+	datapack = stream.NewSimpleDatapack(ctx, rc)
+	hasNext = f.idx < len(f.fileList)
+	return
 }
