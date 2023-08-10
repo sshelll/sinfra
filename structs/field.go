b2cb8df801aba1d7878cd6c49b3497bbba58202b
kayce <kayce@bituniverse.org>
2023-08-10 16:02:46 +0800

    feat: check required with reflect.

--- structs/field.go
+++ structs/field.go
@@ -1,10 +1,12 @@
 package structs
 
-import "reflect"
+import (
+	"reflect"
+	"strings"
+)
 
 // ExtractStructFieldValues this method will extract real val if a field is a non-nil ptr.
 func ExtractStructFieldValues(v reflect.Value) (fvs []interface{}) {
-
 	if !v.IsValid() {
 		panic("reflect value is invalid: " + v.Kind().String())
 	}
@@ -34,4 +36,70 @@ func ExtractStructFieldValues(v reflect.Value) (fvs []interface{}) {
 
 	return
 
+}
+
+func CheckRequired(v reflect.Value, tag string) (missing []string) {
+	if !v.IsValid() {
+		return
+	}
+
+	var check func(name string, v reflect.Value, tag string) (missing []string)
+	check = func(name string, v reflect.Value, tag string) (missing []string) {
+		for v.Kind() == reflect.Ptr {
+			if v.IsNil() {
+				return
+			}
+			v = v.Elem()
+		}
+
+		if v.Kind() != reflect.Struct {
+			return
+		}
+
+		for i, n := 0, v.NumField(); i < n; i++ {
+			fv := v.Field(i)
+			ft := v.Type().Field(i)
+
+			if tagv := ft.Tag.Get(tag); tagv != "true" {
+				continue
+			}
+
+			if ft.Anonymous || ft.Type.Kind() == reflect.Struct {
+				missing = append(missing, check(name+"."+ft.Name, fv, tag)...)
+				continue
+			}
+
+			switch fv.Kind() {
+			case reflect.Invalid:
+				missing = append(missing, name+"."+ft.Name)
+			case reflect.Interface:
+				if fv.IsNil() {
+					missing = append(missing, name+"."+ft.Name)
+				}
+			case reflect.Ptr:
+				if fv.IsNil() {
+					missing = append(missing, name+"."+ft.Name)
+				}
+				missing = append(missing, check(name+"."+ft.Name, fv, tag)...)
+			case reflect.Slice, reflect.Array, reflect.Map:
+				if fv.Len() == 0 {
+					missing = append(missing, name+"."+ft.Name)
+				}
+			}
+		}
+
+		return
+	}
+
+	typeName := v.Type().String()
+	splited := strings.Split(typeName, ".")
+	if len(splited) > 0 {
+		typeName = splited[len(splited)-1]
+	}
+	if len(typeName) == 0 {
+		typeName = "$"
+	}
+	missing = check(typeName, v, tag)
+
+	return
 }
--- structs/field_test.go
+++ structs/field_test.go
@@ -53,3 +53,58 @@ func TestExtractNormalTypeValue(t *testing.T) {
 	assert.Equal(t, 1, len(fvs))
 	assert.Equal(t, 3, fvs[0])
 }
+
+type S1 struct {
+	ID        *string `required:"true"`
+	Name      string
+	Interfase interface{} `required:"true"`
+	Anon      struct {
+		Anon1 *int  `required:"true"`
+		Anon2 []int `required:"true"`
+		Anon3 string
+	} `required:"true"`
+	S2 *S2 `required:"true"`
+}
+
+type S2 struct {
+	Age *int `required:"true"`
+}
+
+func TestCheckRequired(t *testing.T) {
+	s := &S1{
+		ID:   conv.StrPtr("id"),
+		Name: "name",
+		Anon: struct {
+			Anon1 *int  `required:"true"`
+			Anon2 []int `required:"true"`
+			Anon3 string
+		}{
+			Anon1: conv.IntPtr(1),
+			Anon2: []int{1, 2, 3},
+			Anon3: "anon3",
+		},
+		S2: &S2{},
+	}
+
+	missing := CheckRequired(reflect.ValueOf(s), "required")
+	t.Log(missing)
+
+	s.ID = nil
+	missing = CheckRequired(reflect.ValueOf(s), "required")
+	t.Log(missing)
+
+	s.Anon.Anon1 = nil
+	missing = CheckRequired(reflect.ValueOf(s), "required")
+	t.Log(missing)
+
+	s.Anon = struct {
+		Anon1 *int  `required:"true"`
+		Anon2 []int `required:"true"`
+		Anon3 string
+	}{}
+	missing = CheckRequired(reflect.ValueOf(s), "required")
+	t.Log(missing)
+
+	n := reflect.ValueOf(nil)
+	missing = CheckRequired(n, "required")
+}
