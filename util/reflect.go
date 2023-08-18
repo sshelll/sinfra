8a33ddbec1caccf3df147bc0ebc224a0233f3ec0
sshelll <shaojiale.gitignore@icloud.com>
2023-08-18 23:12:11 +0800

    feat: reflect utils.

---
+++ util/reflect.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"reflect"
+)
+
+func Indirect(v reflect.Value) (elem reflect.Value, isNil bool) {
+	for v.Kind() == reflect.Ptr {
+		if v.IsNil() {
+			return v, true
+		}
+		v = v.Elem()
+	}
+	return v, false
+}
+
+func IsNil(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Ptr,
+		reflect.UnsafePointer, reflect.Interface, reflect.Slice:
+		return v.IsNil()
+	}
+	return false
+}
