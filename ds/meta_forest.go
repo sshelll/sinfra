709d190b733ae7bdcfb96dd2d75349a631adc61c
sshelll <shaojiale.gitignore@icloud.com>
2023-08-26 16:03:07 +0800

    feat: build forest.

--- ds/meta_forest.go
+++ ds/meta_forest.go
@@ -6,7 +6,6 @@ type (
 	MetaForest[T any] struct {
 		Children []*MetaTree[T]
 		Extra    any
-		mu       sync.Mutex
 		nodeMap  *sync.Map
 	}
 
@@ -18,10 +17,11 @@ type (
 	MetaNode[T any] struct {
 		// This means a MetaNode is a MetaData
 		MetaData[T]
-		Key      string
-		Val      T
-		Parent   *MetaNode[T]
-		Children []*MetaNode[T]
+		Key       string
+		Val       T
+		ParentKey *string
+		Parent    *MetaNode[T]
+		Children  []*MetaNode[T]
 	}
 
 	MetaData[T any] interface {
@@ -31,12 +31,16 @@ type (
 		GetVal() T
 		GetParent() MetaData[T]
 		GetChildren() []MetaData[T]
+		GetParentKey() *string
+		GetChildrenKeys() []string
 	}
 )
 
+// EnableHashIndex enables hash index for MetaForest.
+// This is useful when you want to search a node by key.
+// NOTE: this function just init the index, it won't build the index,
+// the index will be built when you call SearchNode.
 func (f *MetaForest[T]) EnableHashIndex() {
-	f.mu.Lock()
-	defer f.mu.Unlock()
 	if f.nodeMap != nil {
 		return
 	}
@@ -44,8 +48,6 @@ func (f *MetaForest[T]) EnableHashIndex() {
 }
 
 func (f *MetaForest[T]) ResetHashIndex() {
-	f.mu.Lock()
-	defer f.mu.Unlock()
 	if f.nodeMap == nil {
 		return
 	}
@@ -53,19 +55,14 @@ func (f *MetaForest[T]) ResetHashIndex() {
 }
 
 func (f *MetaForest[T]) DisableHashIndex() {
-	f.mu.Lock()
-	defer f.mu.Unlock()
 	f.nodeMap = nil
 }
 
 func (f *MetaForest[T]) RemoveHashIndex(key string) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
 	f.nodeMap.Delete(key)
 }
 
 func (f *MetaForest[T]) SearchNode(key string) *MetaNode[T] {
-
 	if f == nil {
 		return nil
 	}
@@ -82,21 +79,17 @@ func (f *MetaForest[T]) SearchNode(key string) *MetaNode[T] {
 	}
 
 	return nil
-
 }
 
 func (t *MetaTree[T]) SearchNode(key string) *MetaNode[T] {
-
 	if t == nil || t.Root == nil {
 		return nil
 	}
 
 	return t.Root.SearchNode(key)
-
 }
 
 func (node *MetaNode[T]) SearchNode(key string) *MetaNode[T] {
-
 	if node == nil {
 		return nil
 	}
@@ -112,11 +105,9 @@ func (node *MetaNode[T]) SearchNode(key string) *MetaNode[T] {
 	}
 
 	return nil
-
 }
 
 func (node *MetaNode[T]) ExpandKey() []string {
-
 	keyList := make([]string, 0, 8)
 
 	node.Walk(func(mn *MetaNode[T]) {
@@ -124,11 +115,9 @@ func (node *MetaNode[T]) ExpandKey() []string {
 	})
 
 	return keyList
-
 }
 
 func (node *MetaNode[T]) ExpandVal() []T {
-
 	valList := make([]T, 0, 8)
 
 	node.Walk(func(mn *MetaNode[T]) {
@@ -136,11 +125,9 @@ func (node *MetaNode[T]) ExpandVal() []T {
 	})
 
 	return valList
-
 }
 
 func (node *MetaNode[T]) Walk(cb func(*MetaNode[T])) {
-
 	if node == nil {
 		return
 	}
@@ -151,7 +138,6 @@ func (node *MetaNode[T]) Walk(cb func(*MetaNode[T])) {
 		c := node.Children[i]
 		c.Walk(cb)
 	}
-
 }
 
 func (node *MetaNode[T]) SetKey(key string) {
@@ -182,8 +168,27 @@ func (node *MetaNode[T]) GetChildren() []MetaData[T] {
 	return r
 }
 
-func BuildMetaForestTopDown[T any](metaDataList []MetaData[T]) *MetaForest[T] {
+func (node *MetaNode[T]) GetParentKey() *string {
+	var t *MetaNode[T]
+	if node.Parent != t {
+		k := node.Parent.GetKey()
+		return &k
+	}
+	return node.ParentKey
+}
 
+func (node *MetaNode[T]) GetChildrenKeys() []string {
+	var r []string
+	for _, c := range node.Children {
+		r = append(r, c.GetKey())
+	}
+	return r
+}
+
+// ConvToMetaForest builds a MetaForest from a list of MetaData.
+// This function just conv a list of MetaData to a list of MetaTree,
+// so you can use a list of functions provided by MetaTree and MetaForest.
+func ConvToMetaForest[T any](metaDataList []MetaData[T]) *MetaForest[T] {
 	if len(metaDataList) == 0 {
 		return nil
 	}
@@ -192,15 +197,13 @@ func BuildMetaForestTopDown[T any](metaDataList []MetaData[T]) *MetaForest[T] {
 
 	for i := range metaDataList {
 		metaData := metaDataList[i]
-		treeList = append(treeList, BuildMetaTreeTopDown[T](metaData))
+		treeList = append(treeList, ConvToMetaTree[T](metaData))
 	}
 
 	return &MetaForest[T]{Children: treeList}
-
 }
 
-func BuildMetaTreeTopDown[T any](metaData MetaData[T]) *MetaTree[T] {
-
+func ConvToMetaTree[T any](metaData MetaData[T]) *MetaTree[T] {
 	if metaData == nil {
 		return nil
 	}
@@ -232,5 +235,72 @@ func BuildMetaTreeTopDown[T any](metaData MetaData[T]) *MetaTree[T] {
 	}
 
 	return &MetaTree[T]{Root: root}
+}
 
+func isNil[T comparable](arg T) bool {
+	var t T
+	return arg == t
+}
+
+func ConvToMetaDataList[T any](metaNodes []*MetaNode[T]) []MetaData[T] {
+	var r []MetaData[T]
+	for _, node := range metaNodes {
+		r = append(r, node)
+	}
+	return r
+}
+
+// BuildMetaForestBottomUp builds a MetaForest from a list of MetaData.
+// This function builds the forest bottom up, so each MetaData must have a 'parent key',
+// which means you have to impl GetParentKey(), GetKey(), GetVal() methods.
+// NOTE: GetParent() returns "" means this node is a root.
+func BuildMetaForestBottomUp[T any](metaDataList []MetaData[T]) *MetaForest[T] {
+	var roots []*MetaNode[T]
+	var nodeMap = &sync.Map{}
+
+	// init node map
+	for _, metaData := range metaDataList {
+		key := metaData.GetKey()
+		nodeMap.Store(key, &MetaNode[T]{
+			Key:       metaData.GetKey(),
+			Val:       metaData.GetVal(),
+			ParentKey: metaData.GetParentKey(),
+		})
+	}
+
+	// link nodes to their parents
+	nodeMap.Range(func(key, value any) (continue_ bool) {
+		continue_ = true
+		node := value.(*MetaNode[T])
+
+		// find parent
+		parentKey := node.GetParentKey()
+		if parentKey == nil {
+			roots = append(roots, node)
+			return
+		}
+		p, ok := nodeMap.Load(*parentKey)
+		parent := p.(*MetaNode[T])
+
+		// no parent found, this node is a root
+		if !ok {
+			roots = append(roots, node)
+			return
+		}
+
+		// do link
+		node.Parent = parent
+		parent.Children = append(parent.Children, node)
+		return
+	})
+
+	trees := make([]*MetaTree[T], 0, len(roots))
+	for _, root := range roots {
+		trees = append(trees, &MetaTree[T]{Root: root})
+	}
+
+	return &MetaForest[T]{
+		Children: trees,
+		nodeMap:  nodeMap,
+	}
 }
--- ds/meta_forest_test.go
+++ ds/meta_forest_test.go
@@ -2,44 +2,11 @@ package ds
 
 import (
 	"testing"
+
+	"github.com/sshelll/sinfra/util"
 )
 
-type UnitTestNode struct {
-	k string
-	v string
-	p *UnitTestNode
-	c []*UnitTestNode
-}
-
-func (n *UnitTestNode) SetKey(k string) {
-	n.k = k
-}
-
-func (n *UnitTestNode) GetKey() string {
-	return n.k
-}
-
-func (n *UnitTestNode) SetVal(v string) {
-	n.v = v
-}
-
-func (n *UnitTestNode) GetVal() string {
-	return n.v
-}
-
-func (n *UnitTestNode) GetParent() MetaData[string] {
-	return n.p
-}
-
-func (n *UnitTestNode) GetChildren() []MetaData[string] {
-	var r []MetaData[string]
-	for _, c := range n.c {
-		r = append(r, c)
-	}
-	return r
-}
-
-func TestBuildTree(t *testing.T) {
+func TestConvToTree(t *testing.T) {
 	/*
 		root
 		├── child-1
@@ -51,53 +18,141 @@ func TestBuildTree(t *testing.T) {
 		├── child-2
 		└── child-3
 	*/
-	utRoot := &UnitTestNode{
-		k: "root",
-		v: "root-val",
-		c: []*UnitTestNode{
+	utRoot := &MetaNode[string]{
+		Key: "root",
+		Val: "root-val",
+		Children: []*MetaNode[string]{
 			{
-				k: "child-1",
-				v: "child-1-val",
-				c: []*UnitTestNode{
+				Key: "child-1",
+				Val: "child-1-val",
+				Children: []*MetaNode[string]{
 					{
-						k: "child-1-1",
-						v: "child-1-1-val",
-						c: []*UnitTestNode{
+						Key: "child-1-1",
+						Val: "child-1-1-val",
+						Children: []*MetaNode[string]{
 							{
-								k: "child-1-1-1",
-								v: "child-1-1-1-val",
+								Key: "child-1-1-1",
+								Val: "child-1-1-1-val",
 							},
 							{
-								k: "child-1-1-2",
-								v: "child-1-1-2-val",
+								Key: "child-1-1-2",
+								Val: "child-1-1-2-val",
 							},
 						},
 					},
 					{
-						k: "child-1-2",
-						v: "child-1-2-val",
-						c: []*UnitTestNode{
+						Key: "child-1-2",
+						Val: "child-1-2-val",
+						Children: []*MetaNode[string]{
 							{
-								k: "child-1-2-1",
-								v: "child-1-2-1-val",
+								Key: "child-1-2-1",
+								Val: "child-1-2-1-val",
 							},
 						},
 					},
 				},
 			},
 			{
-				k: "child-2",
-				v: "child-2-val",
+				Key: "child-2",
+				Val: "child-2-val",
 			},
 			{
-				k: "child-3",
-				v: "child-3-val",
+				Key: "child-3",
+				Val: "child-3-val",
 			},
 		},
 	}
-	tree := BuildMetaTreeTopDown(utRoot)
+	tree := ConvToMetaTree(utRoot)
 	keys := tree.Root.ExpandKey()
 	vals := tree.Root.ExpandVal()
 	t.Logf("keys: %v", keys)
 	t.Logf("vals: %v", vals)
+}
+
+// This refers to real biz entity.
+type TestUser struct {
+	name string
+	age  int
+}
+
+// This is a wrapper of real biz entity.
+type TestUserWrapper struct {
+	info     *TestUser
+	id       string
+	parentID *string
+}
+
+func (u *TestUserWrapper) SetKey(key string) {
+	panic("implement me")
+}
+
+func (u *TestUserWrapper) GetKey() string {
+	return u.id
+}
+
+func (u *TestUserWrapper) SetVal(val *TestUser) {
+	panic("implement me")
+}
+
+func (u *TestUserWrapper) GetVal() *TestUser {
+	return u.info
+}
+
+func (u *TestUserWrapper) GetParent() MetaData[*TestUser] {
+	panic("implement me")
+}
+
+func (u *TestUserWrapper) GetChildren() []MetaData[*TestUser] {
+	panic("implement me")
+}
+
+func (u *TestUserWrapper) GetParentKey() *string {
+	return u.parentID
+}
+
+func (u *TestUserWrapper) GetChildrenKeys() []string {
+	panic("implement me")
+}
+
+func TestRealCase(t *testing.T) {
+	leaderA := &TestUserWrapper{
+		id: "a",
+	}
+	leaderB := &TestUserWrapper{
+		id: "b",
+	}
+	leaderC := &TestUserWrapper{
+		id: "c",
+	}
+	workerA1 := &TestUserWrapper{
+		id:       "a1",
+		parentID: util.Ptr(leaderA.id),
+	}
+	workerA2 := &TestUserWrapper{
+		id:       "a2",
+		parentID: util.Ptr(leaderA.id),
+	}
+	workerB1 := &TestUserWrapper{
+		id:       "b1",
+		parentID: util.Ptr(leaderB.id),
+	}
+	workerB11 := &TestUserWrapper{
+		id:       "b11",
+		parentID: util.Ptr(workerB1.id),
+	}
+	users := []MetaData[*TestUser]{
+		leaderA,
+		leaderB,
+		leaderC,
+		workerA1,
+		workerA2,
+		workerB1,
+		workerB11,
+	}
+	forest := BuildMetaForestBottomUp(users)
+	t.Logf("forest tree cnt: %v", len(forest.Children))
+	t.Log(forest.Children[0].Root.ExpandKey())
+	t.Log(forest.Children[1].Root.ExpandKey())
+	t.Log(forest.Children[2].Root.ExpandKey())
+	t.Log(forest.SearchNode("b1").ExpandKey())
 }
